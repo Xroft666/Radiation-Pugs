1761e061edf2623e6d0d242cb37a1962935083ac
Miki Anthony <miki_anthony@hotmail.com>
2016-04-09 00:47:08 +0200

    Moving pugs!!! \o/

--- Project/Assets/Scripts/Moving.cs
+++ Project/Assets/Scripts/Moving.cs
@@ -6,9 +6,19 @@ public class Moving : MonoBehaviour
 
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+		float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
+		/*
         transform.position += new Vector3(x, y) * Time.deltaTime;
+*/
+		if(x != 0 || y != 0)
+		{
+			float direction = Mathf.Rad2Deg * Mathf.Atan2(-x,y);
+
+			transform.eulerAngles = new Vector3(0,0,direction);
+		}
+
+		transform.position += transform.up * Time.deltaTime;
     }
 }
