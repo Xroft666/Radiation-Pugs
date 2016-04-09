a4f32e2fcc52ec177bad2102c020b1bc29b8434e
Miki Anthony <miki_anthony@hotmail.com>
2016-04-09 22:33:13 +0200

    Depth

---
+++ Project/Assets/Scripts/DepthOrder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class DepthOrder : MonoBehaviour {
+
+	private SpriteRenderer cachedSpriteRenderer;
+	public float Size = 0.4f; 
+
+	// Use this for initialization
+	void Start () {
+		cachedSpriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cachedSpriteRenderer.sortingOrder = -Mathf.RoundToInt((transform.position.y - Size/2) * 100);
+	}
+}
