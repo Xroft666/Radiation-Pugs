35b471ca58beee7e8f1547f3a7b2462ecc7a3bc8
Miki Anthony <miki_anthony@hotmail.com>
2016-04-09 19:15:42 +0200

    LoadLevel fix

--- Project/Assets/Scripts/loadLevel.cs
+++ Project/Assets/Scripts/loadLevel.cs
@@ -5,7 +5,7 @@ public class loadLevel : MonoBehaviour {
 
 	void Update()
 	{
-		if(Input.GetKey(KeyCode.Z))
+		if(Input.GetKey(KeyCode.Z) || Input.GetButton("Joy1" + " Pee") || Input.GetButton("Joy2" + " Pee") || Input.GetButton("Joy3" + " Pee") || Input.GetButton("Joy4" + " Pee"))
 		{
 			LoadStage();
 		}
@@ -13,6 +13,8 @@ public class loadLevel : MonoBehaviour {
 
 	public void LoadStage()  {
 		Application.LoadLevel (1);
+		States.won = false;
+		States.isStart = true;
 	}
 
 }
